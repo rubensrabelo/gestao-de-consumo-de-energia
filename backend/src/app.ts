f8ff8750df4e816a14e7cb716aa183bc2c9b42f0
Rubens_Rabelo <rubensrabelo123@gmail.com>
2026-01-10 19:13:48 -0300

    feat(backend): enable request via CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { connectMongo } from "./infra/database/mongo";
 import energyRoutes from "./api/routes/energy.routes";
@@ -8,6 +9,8 @@ import { dashboardRoutes } from "./api/routes/dashboard.routes";
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
